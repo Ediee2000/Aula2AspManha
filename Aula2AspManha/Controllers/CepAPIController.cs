75cb829bb5da4517b476003d4db700c7e19d9b4f
Ediee <ediee2000@gmail.com>
2020-07-20 19:30:54 -0300

    Release API

---
+++ Aula2AspManha/Controllers/CepAPIController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Aula2AspManha.DAL;
+using Aula2AspManha.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Aula2AspManha.Controllers
+{
+    [Route("api/Endereco")]
+    [ApiController]
+    public class CepAPIController : ControllerBase
+    {
+        private readonly AddressDAO DAOLayer;
+
+        public CepAPIController(AddressDAO _DAOLayer)
+        {
+            DAOLayer = _DAOLayer;
+        }
+
+        [HttpGet]
+        [Route("ListarEnderecos")]
+        public List<Address> ListarEnderecos()
+        {
+            return DAOLayer.ListAddress();
+        }
+
+        [HttpGet("{cep}")]
+        [Route("ListarEnderecos/{cep}")]
+        public List<Address> ListaEnderecos(string cep)
+        {
+            return DAOLayer.GetAddress(cep);
+        }
+
+        [HttpPost]
+        [Route("Cadastrar")]
+        public IActionResult CadastrarEndereco(Address address)
+        {
+            DAOLayer.SaveAddress(address);
+            return Created("", address);
+        }
+
+        [HttpPut]
+        [Route("AlterarEndereco")]
+        public IActionResult EditAddress(Address address)
+        {
+            return Created("",DAOLayer.ModifyAddress(address));
+        }
+
+        [HttpGet("{id}")]
+        [Route("DeletarEndereco/{id}")]
+        public IActionResult DeleteAddress(int id)
+        {
+            if (DAOLayer.DeleteAddress(id))
+            {
+               return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- Aula2AspManha/DAL/AddressDAO.cs
+++ Aula2AspManha/DAL/AddressDAO.cs
@@ -1,9 +1,12 @@
 ﻿using Aula2AspManha.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+//Ediee Pinheiro
 
 namespace Aula2AspManha.DAL
 {
@@ -23,6 +26,12 @@ namespace Aula2AspManha.DAL
             return DbContext.Enderecos.ToList();
         }
 
+        public List<Address> GetAddress(string Cep)
+        {
+            Cep = Cep.Substring(0, 5) + "-" + Cep.Substring(5, 3);
+            return DbContext.Enderecos.Where(b => b.Cep == Cep).ToList<Address>();
+        }
+
         public bool SaveAddress(Address newAddress)
         {
             if (newAddress.Cep != null)
@@ -32,6 +41,39 @@ namespace Aula2AspManha.DAL
                 return true;
             }
             else
+            {
+                return false;
+            }
+        }
+
+        public Address ModifyAddress(Address ModifAdd)
+        {
+            Address Temp = DbContext.Enderecos.Single(b => b.Id == ModifAdd.Id);
+            Temp.Cep = ModifAdd.Cep;
+            Temp.Logradouro = ModifAdd.Logradouro;
+            Temp.Bairro = ModifAdd.Bairro;
+            Temp.Localidade = ModifAdd.Localidade;
+            Temp.Uf = ModifAdd.Uf;
+            Temp.Unidade = ModifAdd.Unidade;
+            Temp.Ibge = ModifAdd.Ibge;
+            Temp.Gia = ModifAdd.Gia;
+            Temp.Complemento = ModifAdd.Complemento;
+
+            DbContext.SaveChanges();
+
+            return Temp;
+        }
+
+        public bool DeleteAddress(int id)
+        {
+            try
+            {
+                Address Temp = DbContext.Enderecos.Single(b => b.Id == id);
+                DbContext.Enderecos.Remove(Temp);
+                DbContext.SaveChanges();
+                return true;
+            }
+            catch
             {
                 return false;
             }
